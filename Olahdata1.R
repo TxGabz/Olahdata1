48fe715abf1d25c100ba292bc1135cc90e6b2353
TxGabz <44201252+TxGabz@users.noreply.github.com>
2024-11-08 08:47:38 +0700

    Update Olahdata1.R
    Total recode, to not require any libraries when running the code.

--- Olahdata1.R
+++ Olahdata1.R
@@ -1,39 +1,29 @@
-# Sales Analysis Script
-
-# Load necessary libraries
-library(dplyr)
-library(ggplot2)
-
 # Load the data
-data <- read.csv("C:\Users\PC-Labcom-11\Downloads\supermarket_sales.csv")
+data <- read.csv("C:/Users/PC-Labcom-11/Downloads/supermarket sales.csv")
 
 # Total sales by branch
-sales_by_branch <- data %>%
-  group_by(Branch) %>%
-  summarise(Total_Sales = sum(Total))
+sales_by_branch <- aggregate(data$Total, by = list(data$Branch), FUN = sum)
+colnames(sales_by_branch) <- c("Branch", "Total_Sales")
+print("Total Sales by Branch:")
+print(sales_by_branch)
 
 # Total sales by product line
-sales_by_product_line <- data %>%
-  group_by(Product.line) %>%
-  summarise(Total_Sales = sum(Total))
+sales_by_product_line <- aggregate(data$Total, by = list(data$Product.line), FUN = sum)
+colnames(sales_by_product_line) <- c("Product_line", "Total_Sales")
+print("Total Sales by Product Line:")
+print(sales_by_product_line)
 
 # Total sales by customer type and gender
-sales_by_customer_gender <- data %>%
-  group_by(Customer.type, Gender) %>%
-  summarise(Total_Sales = sum(Total))
+sales_by_customer_gender <- aggregate(data$Total, by = list(data$Customer.type, data$Gender), FUN = sum)
+colnames(sales_by_customer_gender) <- c("Customer_Type", "Gender", "Total_Sales")
+print("Total Sales by Customer Type and Gender:")
+print(sales_by_customer_gender)
 
-# Plot: Sales by Branch
-ggplot(sales_by_branch, aes(x = Branch, y = Total_Sales, fill = Branch)) +
-  geom_bar(stat = "identity") +
-  labs(title = "Total Sales by Branch", x = "Branch", y = "Total Sales")
-
-# Plot: Sales by Product Line
-ggplot(sales_by_product_line, aes(x = reorder(Product.line, Total_Sales), y = Total_Sales, fill = Product.line)) +
-  geom_bar(stat = "identity") +
-  labs(title = "Total Sales by Product Line", x = "Product Line", y = "Total Sales") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-# Plot: Sales by Customer Type and Gender
-ggplot(sales_by_customer_gender, aes(x = Customer.type, y = Total_Sales, fill = Gender)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  labs(title = "Sales by Customer Type and Gender", x = "Customer Type", y = "Total Sales")
+# Display insights in the console
+cat("\n--- Summary ---\n")
+cat("\nTotal Sales by Branch:\n")
+print(sales_by_branch)
+cat("\nTotal Sales by Product Line:\n")
+print(sales_by_product_line)
+cat("\nTotal Sales by Customer Type and Gender:\n")
+print(sales_by_customer_gender)
